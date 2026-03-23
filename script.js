b4b28e216b5f63f613ca2fc60bc3b7be310d2092
MarcoMTG <marcotuliomm.s45@gmail.com>
2026-03-23 16:57:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-const boton = document.getElementById('btnConectar');
+const boton = document.getElementById('btnEjecutar');
 const caja = document.getElementById('resultado');
 
 boton.addEventListener('click', async () => {
+    const passInput = document.getElementById('claveUsuario').value;
+    
+    if (!passInput) {
+        alert("¡Falta la clave de la ESCOM!");
+        return;
+    }
+
     caja.innerHTML = "Llamando a la API...";
+
     try {
-        // Esto busca la carpeta secreta que haremos después
         const res = await fetch('/api/conectar');
         const data = await res.json();
-        caja.innerHTML = "Mensaje del servidor: " + data.mensaje;
+        
+        // --- Algoritmo de Matrices Inversas ---
+        // Usamos la clave que escribiste en el iPad para desencriptar
+        const fA = passInput.charCodeAt(0); // ASCII de la primera letra
+        const fB = passInput.length;       // Largo de la clave
+
+        let tituloReal = data.datosCifrados.map(num => {
+            // Operación inversa matemática
+            let codigoASCII = (num - fB) / fA;
+            return String.fromCharCode(codigoASCII);
+        }).join('');
+
+        caja.innerHTML = `Mensaje: ${data.mensaje} <br> Título: <strong>${tituloReal}</strong>`;
     } catch (err) {
-        caja.innerHTML = "Error al conectar.";
+        caja.innerHTML = "Error: Revisa tu conexión o la IP en MongoDB.";
     }
 });
