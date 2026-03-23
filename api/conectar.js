3436d57e1ce170c9732ad9ccdd2278c4efca8536
MarcoMTG <marcotuliomm.s45@gmail.com>
2026-03-23 16:58:08 -0600

    Update conectar.js

--- api/conectar.js
+++ api/conectar.js
@@ -1,21 +1,29 @@
 import { MongoClient } from 'mongodb';
 
 export default async function handler(req, res) {
+    // Jalamos la clave desde la "Caja Fuerte" de Vercel (Paso 1)
+    const passBackend = process.env.CLAVE_MATRIZ;
     const cliente = new MongoClient(process.env.MONGODB_URI);
+    
     try {
         await cliente.connect();
         const db = cliente.db("sample_mflix");
-        const coleccion = db.collection("movies");
+        const pelicula = await db.collection("movies").findOne({});
         
-        // Buscamos la película de la foto que me mandaste
-        const pelicula = await coleccion.findOne({});
+        // Encriptamos antes de mandar el dato a la web
+        const fA = passBackend.charCodeAt(0);
+        const fB = passBackend.length;
         
+        let cifrado = pelicula.title.split('').map(char => {
+            return (char.charCodeAt(0) * fA) + fB;
+        });
+
         res.status(200).json({ 
-            mensaje: "¡Conexión Exitosa!",
-            titulo: pelicula.title
+            mensaje: "Datos Cifrados con Matriz",
+            datosCifrados: cifrado 
         });
     } catch (e) {
-        res.status(500).json({ error: "Error de conexión" });
+        res.status(500).json({ error: "Fallo de conexión" });
     } finally {
         await cliente.close();
     }
